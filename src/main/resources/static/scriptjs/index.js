42853298541fd4f8783ea78e10f3b8f6d6e0435b
teste2811 <armarianam12@gmail.com>
2025-01-26 13:52:45 -0300

    integracao com o front end de excluir lista

--- src/main/resources/static/scriptjs/index.js
+++ src/main/resources/static/scriptjs/index.js
@@ -4,19 +4,38 @@ document.addEventListener('DOMContentLoaded', exibirListas);
 
 async function exibirListas() {
     const collectionDiv = document.querySelector('#collection');
-    collectionDiv.innerHTML = '';
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
+        collectionDiv.innerHTML = '';
 
-        // Adiciona as listas ao DOM
         data.forEach(item => {
             const p = document.createElement('p');
-            p.textContent = item.name;
-            p.setAttribute("data-id", item.id);
+            const img_trash = document.createElement('img')
+            const img_pencil = document.createElement('img')
+            const divIcons = document.createElement('div');
+
+
+            divIcons.classList.add("icons")
             p.classList.add("title-collection");
+            img_trash.classList.add("trash-icon")
+            img_pencil.classList.add("pencil-icon")
+
+            p.textContent = item.name;
+
+            img_pencil.src = "..//static/icons/icons8-lápis-30.png";
+            img_trash.src = "..//static/icons/icons8-trash-24.png";
+
+            img_pencil.setAttribute("data-id",item.id)
+            img_trash.setAttribute("data-id",item.id)
+            p.setAttribute("data-id", item.id);
+
+            divIcons.appendChild(img_trash);
+            divIcons.appendChild(img_pencil);
+
 
             collectionDiv.appendChild(p);
+            collectionDiv.appendChild(divIcons)
         });
 
         const collections = document.querySelectorAll(".title-collection");
@@ -26,9 +45,28 @@ async function exibirListas() {
                 window.location.href = `games.html?listID=${listID}`;
             });
         });
+
+        const trashIcons = document.querySelectorAll(".trash-icon");
+        trashIcons.forEach(icon => {
+            icon.addEventListener("click", () => excluirLista(icon.getAttribute("data-id")));        })
+
+        const pencilIcons = document.querySelectorAll(".pencil-icon");
+        pencilIcons.forEach(icon => {
+            icon.addEventListener("click", () => alert("dev"));
+        })
     } catch (error) {
-        const h3 = document.createElement('h3');
-        h3.textContent = 'Erro ao carregar listas';
-        collectionDiv.appendChild(h3);
+        
+    }
+}
+
+async function excluirLista(id) {
+    try {
+        const response = await fetch(apiUrl+`/${id}`,{
+            method: 'DELETE'
+        })
+        window.location.reload();
+
+    } catch (error) {
+        console.log(error)
     }
 }
